6c962febbceceb1cf9289aa942aeee4ef6c7875e
caio.mijoler <caio.mijoler@gmai.com>
2024-12-07 20:55:50 -0300

    adjust to get token

--- src/services/interceptors/AuthInterceptor.ts
+++ src/services/interceptors/AuthInterceptor.ts
@@ -1,18 +1,15 @@
-'use client'
-
 import { getCookie } from '@/app/actions'
 import { InternalAxiosRequestConfig } from 'axios'
 
 export const authInterceptor = async (config: InternalAxiosRequestConfig) => {
-  const { token } = await getCookie('authToken')
-  console.log('token', token)
+  const res = await getCookie('authToken')
 
-  if (process.env.NODE_ENV !== 'development' && !token) {
+  if (process.env.NODE_ENV !== 'development' && !res?.token) {
     window.location.href = '/'
   }
 
-  if (token && config.headers) {
-    config.headers.Authorization = `Bearer ${token}`
+  if (res?.token && config.headers) {
+    config.headers.Authorization = `Bearer ${res?.token}`
   }
   return config
 }

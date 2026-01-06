1cd50ac7ccb37b6d2db7f9f0a340c6cc13238039
gabrielmijoler <gabrielmijoler@hotmail.com>
2026-01-06 20:25:31 -0300

    fix: removi

--- src/componentNext/empty/empty.tsx
+++
@@ -1,7 +0,0 @@
-export default function EmptyComponent() {
-  return (
-    <div className="flex flex-col gap-2 w-full justify-center items-center">
-      <p>Nenhuma empresa encontrada, tente novamente!</p>
-    </div>
-  );
-}
